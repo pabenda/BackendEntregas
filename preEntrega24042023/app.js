7f26b2bfed25a620198ddb7b99df06aa4f323ed4
Pablo D. Bendayan <pablodb@gmail.com>
2023-04-26 11:16:58 -0300

    se modifica segun parametros entrega

---
+++ preEntrega24042023/app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const ProductManager = require("./productManager");
+
+const productManager = new ProductManager("./products.json");
+
+// Verificación 1: Para obtener todos los productos, llame a http://localhost:8080/products sin query.
+app.get("/products", async (req, res) => {
+  try {
+    const limit = req.query.limit ? parseInt(req.query.limit) : undefined;
+    const products = await productManager.getProducts(limit);
+    res.json(products);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Verificación 2: Para obtener los primeros "limit" productos, llame a http://localhost:8080/products?limit=5.
+app.get("/products", async (req, res) => {
+  try {
+    const limit = req.query.limit ? parseInt(req.query.limit) : undefined;
+    const products = await productManager.getProducts(limit);
+    res.json(products);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Verificación 3: Para obtener un solo producto con el id especificado, llame a http://localhost:8080/products/2, donde "2" es el id del producto deseado.
+app.get("/products/:pid", async (req, res) => {
+  try {
+    const productId = parseInt(req.params.pid);
+    const product = await productManager.getProductById(productId);
+    if (!product) {
+      res.status(404).send(`Product with id ${productId} not found`);
+    } else {
+      res.json(product);
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Verificación 4: Para obtener un objeto de error si el producto no existe, llame a http://localhost:8080/products/34123123, donde "34123123" es un id de producto inexistente.
+app.get("/products/:pid", async (req, res) => {
+  try {
+    const productId = parseInt(req.params.pid);
+    const product = await productManager.getProductById(productId);
+    if (!product) {
+      res.status(404).json({ error: `Product with id ${productId} not found` });
+    } else {
+      res.json(product);
+    }
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
