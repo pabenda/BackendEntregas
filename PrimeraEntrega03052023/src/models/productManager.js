215589491742b9f5c0347eafe0414033986108f1
Pablo D. Bendayan <pablodb@gmail.com>
2023-05-06 16:30:14 -0300

    subida de primera version

---
+++ PrimeraEntrega03052023/src/models/productManager.js
@@ -0,0 +1,56 @@
+const fs = require("fs").promises;
+
+class ProductManager {
+  constructor(path) {
+    this.path = path;
+    this._idCounter = 0;
+  }
+
+  async addProduct(product) {
+    product.id = ++this._idCounter;
+    let products = await this.getProducts();
+    products.push(product);
+    await fs.writeFile(this.path, JSON.stringify(products));
+  }
+
+  async getProducts(limit) {
+    let products = [];
+    try {
+      const file = await fs.readFile(this.path);
+      products = JSON.parse(file);
+    } catch (error) {
+      console.log(error);
+    }
+    if (limit !== undefined) {
+      return products.slice(0, limit);
+    }
+    return products;
+  }
+
+  async getProductById(id) {
+    let products = await this.getProducts();
+    return products.find((product) => product.id === id);
+  }
+
+  async updateProduct(id, newProduct) {
+    let products = await this.getProducts();
+    let productIndex = products.findIndex((product) => product.id === id);
+    if (productIndex !== -1) {
+      newProduct.id = id;
+      products[productIndex] = newProduct;
+      await fs.writeFile(this.path, JSON.stringify(products));
+    }
+  }
+
+  async deleteProduct(id) {
+    let products = await this.getProducts();
+    let filteredProducts = products.filter((product) => product.id !== id);
+    if (products.length !== filteredProducts.length) {
+      await fs.writeFile(this.path, JSON.stringify(filteredProducts));
+    } else {
+      throw new Error(`Producto con ID ${id} no existe`);
+    }
+  }
+}
+
+module.exports = ProductManager;
